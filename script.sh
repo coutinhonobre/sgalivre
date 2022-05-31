04f82f1b9183ec532db031881d2cfb73a260778c
Igor Nobre <ferreiranobre@outlook.com>
2022-05-30 23:55:11 -0300

    install postgres

--- script.sh
+++ script.sh
@@ -4,8 +4,6 @@ sudo apt -y install apache2 &&
 sudo apt -y install php-cli php-fpm php-json php-pdo php-mysql php-pgsql libapache2-mod-php &&
 sudo systemctl restart apache2 &&
 sudo apt install postgresql postgresql-contrib &&
-sudo systemctl start PostgreSQL &&
-sudo systemctl enable PostgreSQL &&
 sudo wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qyXjS51_A-iAHHMnGnVDMi2toVupu9ZC' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qyXjS51_A-iAHHMnGnVDMi2toVupu9ZC" -O sgalivre.zip && rm -rf /tmp/cookies.txt &&
 sudo unzip sgalivre.zip &&
 sudo cp -R sgalivre /var/www/html/ &&
