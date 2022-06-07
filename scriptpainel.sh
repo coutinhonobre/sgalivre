d3862887d98c89a3e09a9ddea80852da95bab0ed
Igor Nobre <ferreiranobre@outlook.com>
2022-06-07 01:52:24 -0300

    adicionado novo jar

--- scriptpainel.sh
+++ scriptpainel.sh
@@ -2,4 +2,6 @@ sudo apt install default-jre default-jdk &&
 sudo mv sgalivre-painel /usr/share/sgalivre-painel &&
 sudo chmod +x /usr/share/sgalivre-painel/painel.jar &&
 sudo ln -s /usr/share/sgalivre-painel/painel.jar /usr/local/bin/painel.jar &&
-sudo mv /usr/share/sgalivre-painel/sgalivre-painel.desktop ~/.local/share/applications/
+sudo mv /usr/share/sgalivre-painel/sgalivre-painel.desktop /usr/share/applications/ &&
+sudo apt-get install libcanberra-gtk-module &&
+sudo apt install openjdk-17-jdk 
